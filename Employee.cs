314a962a7296132e41af054d89493b0e14e84b2b
Shubham20006 <shubhamchaudhari1506@gmail.com>
2022-12-26 21:57:06 +0530

    Calculating wages of 100hours and 20days

--- Employee.cs
+++ Employee.cs
@@ -6,53 +6,54 @@ using System.Threading.Tasks;
 
 namespace EmployeeWageProblem
 {
-    public class Employee 
+    public class Employee
     {
 
         public static void EmpComputation()
         {
-            int Is_Full_Time = 1;
-            int Is_Part_Time = 2;
+           
             int Wage_Per_Hour = 20;
-            int Working_Days_IN_Month = 20;
-            int FullDayHour = 8;
-            int DailyEmpWage;
-            int MonthlyEmpWages;
+            int Max_Days_IN_Month = 20;
+            int Max_HrsInMonth = 100;
+            int empHour;
+            int totalHours = 0;
+            int totalDays = 0;
             Random R = new Random();
             int empCheck = R.Next(3);
+            while (totalHours < Max_HrsInMonth && totalDays < Max_Days_IN_Month)
+            {
+                totalDays++;
+                switch (empCheck)
+                {
+                    case 1:
+                        {
+                            //Console.WriteLine("Employee is present.");
+                            empHour = 8;
+                            break;
+                        }
+                    case 2:
+                        {
+                            //Console.WriteLine("Employee present PartTime.");
+                            empHour = 4;
+                            break;
+                        }
+                    default:
+                        {
+                            //Console.WriteLine("Employee is absent.");
+                            empHour = 0;
+                            break;
+                        }
+                }
+                totalHours = (totalHours + empHour);
 
-            switch (empCheck) {
-                case 1:    
-            {
-                Console.WriteLine("Employee is present.");
-                FullDayHour = 8;
-                DailyEmpWage = Wage_Per_Hour * FullDayHour;
-                Console.WriteLine("Daily employee wage is :- " + DailyEmpWage);
-                MonthlyEmpWages = DailyEmpWage * Working_Days_IN_Month;
-                Console.WriteLine("Monthly employee wage is :- " + MonthlyEmpWages);
-                        break;
+
+                Console.WriteLine("Total employee hours are :- " + totalHours);
+                int sallery = totalHours * totalDays * Wage_Per_Hour;
+                Console.WriteLine("Total employee wage is :- " + sallery);
+                Console.WriteLine("\n");
+
             }
-                case 2:
-            {
-                Console.WriteLine("Employee present PartTime.");
-                int PartTimeHour = 4;
-                DailyEmpWage = Wage_Per_Hour * PartTimeHour;
-                Console.WriteLine("Daily employee wage of Part time  is :- " + DailyEmpWage);
-                MonthlyEmpWages = DailyEmpWage * Working_Days_IN_Month;
-                Console.WriteLine("Monthly employee wage is :- " + MonthlyEmpWages);
-                        break;
-            }
-                default:
-            {
-                Console.WriteLine("Employee is absent.");
-                FullDayHour = 0;
-                DailyEmpWage = Wage_Per_Hour * FullDayHour;
-                Console.WriteLine("Daily employee wage is :- " + DailyEmpWage);
-                MonthlyEmpWages = DailyEmpWage * Working_Days_IN_Month;
-                Console.WriteLine("Monthly employee wage is :- " + MonthlyEmpWages);
-                        break;
-            }
-        }
+
         }
     }
 }
