45079d9876bf7aac646dcc549495fd41a7d1b4aa
Shubham20006 <shubhamchaudhari1506@gmail.com>
2022-12-26 14:05:47 +0530

    Daily emp wages with Part time

--- Employee.cs
+++ Employee.cs
@@ -12,23 +12,33 @@ namespace EmployeeWageProblem
         public static void EmpComputation()
         {
             int Is_Full_Time = 1;
+            int Is_Part_Time = 2;
             int Wage_Per_Hour=20;
             int FullDayHour = 8;
+            int DailyEmpWage;
             Random R = new Random();
-            int empCheck = R.Next(2);
+            int empCheck = R.Next(3);
             if(empCheck==Is_Full_Time)
             {
                 Console.WriteLine("Employee is present.");
                  FullDayHour = 8;
-
+                 DailyEmpWage = Wage_Per_Hour * FullDayHour;
+                Console.WriteLine("Daily employee wage is :- " + DailyEmpWage);
+            }
+            else if(empCheck==Is_Part_Time)
+            {
+                Console.WriteLine("Employee present PartTime.");
+                int PartTimeHour = 4;
+                 DailyEmpWage = Wage_Per_Hour * PartTimeHour;
+                Console.WriteLine("Daily employee wage of Part time  is :- " + DailyEmpWage);
             }
             else
             {
                 Console.WriteLine("Employee is absent.");
-                 FullDayHour = 0;
+                FullDayHour = 0;
+                 DailyEmpWage = Wage_Per_Hour * FullDayHour;
+                Console.WriteLine("Daily employee wage is :- " + DailyEmpWage);
             }
-            int DailyEmpWage = Wage_Per_Hour * FullDayHour;
-            Console.WriteLine("Daily employee wage is :- "+DailyEmpWage);
         }
     }
 }
