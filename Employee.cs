6e9f887fc4b0d28937c4321ad9321e143f8d219a
Shubham20006 <shubhamchaudhari1506@gmail.com>
2022-12-26 14:15:48 +0530

    Daily emp wages with Part time Using Switch Case

--- Employee.cs
+++ Employee.cs
@@ -8,37 +8,43 @@ namespace EmployeeWageProblem
 {
     public class Employee 
     {
-       
+
         public static void EmpComputation()
         {
             int Is_Full_Time = 1;
             int Is_Part_Time = 2;
-            int Wage_Per_Hour=20;
+            int Wage_Per_Hour = 20;
             int FullDayHour = 8;
             int DailyEmpWage;
             Random R = new Random();
             int empCheck = R.Next(3);
-            if(empCheck==Is_Full_Time)
+
+            switch (empCheck) {
+                case 1:    
             {
                 Console.WriteLine("Employee is present.");
-                 FullDayHour = 8;
-                 DailyEmpWage = Wage_Per_Hour * FullDayHour;
+                FullDayHour = 8;
+                DailyEmpWage = Wage_Per_Hour * FullDayHour;
                 Console.WriteLine("Daily employee wage is :- " + DailyEmpWage);
+                        break;
             }
-            else if(empCheck==Is_Part_Time)
+                case 2:
             {
                 Console.WriteLine("Employee present PartTime.");
                 int PartTimeHour = 4;
-                 DailyEmpWage = Wage_Per_Hour * PartTimeHour;
+                DailyEmpWage = Wage_Per_Hour * PartTimeHour;
                 Console.WriteLine("Daily employee wage of Part time  is :- " + DailyEmpWage);
+                        break;
             }
-            else
+                default:
             {
                 Console.WriteLine("Employee is absent.");
                 FullDayHour = 0;
-                 DailyEmpWage = Wage_Per_Hour * FullDayHour;
+                DailyEmpWage = Wage_Per_Hour * FullDayHour;
                 Console.WriteLine("Daily employee wage is :- " + DailyEmpWage);
+                        break;
             }
+        }
         }
     }
 }
