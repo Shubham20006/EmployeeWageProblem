161476aa0ca469c87b974128736df5aa1285b22b
Shubham20006 <120436284+Shubham20006@users.noreply.github.com>
2022-12-30 13:24:48 +0530

    Update Employee.cs

--- Employee.cs
+++ Employee.cs
@@ -11,42 +11,47 @@ namespace EmployeeWageProblem
 
         public static void EmpComputation()
         {
-            int Is_Full_Time = 1;
+             int Is_Full_Time = 1;
             int Is_Part_Time = 2;
             int Wage_Per_Hour = 20;
             int Working_Days_IN_Month = 20;
             int empHour;
             int MonthlyEmpWages;
-            Random R = new Random();
-            int empCheck = R.Next(3);
-         for(int i=0;i<Working_Days_IN_Month;i++)
-         {
-             switch (empCheck) {
-                case 1:    
+            int TotalHrs = 0;
+
+
+            for (int i = 0; i < Working_Days_IN_Month; i++)
             {
-                Console.WriteLine("Employee is present.");
-                empHour = 8;
-                           break;
-            }
-                case 2:
-            {
-                Console.WriteLine("Employee present PartTime.");
-                empHour = 4;
-                     break;
-            }
-                default:
-            {
-                Console.WriteLine("Employee is absent.");
-                empHour = 0;
-                   break;
-            }
-                     
+                Random R = new Random();
+                int empCheck = R.Next(3);
+                switch (empCheck)
+                {
+                    case 1:
+                        {
+                            //  Console.WriteLine("Employee is present.");
+                            empHour = 8;
+                            break;
+                        }
+                    case 2:
+                        {
+                            // Console.WriteLine("Employee present PartTime.");
+                            empHour = 4;
+                            break;
+                        }
+                    default:
+                        {
+                            //  Console.WriteLine("Employee is absent.");
+                            empHour = 0;
+                            break;
+                        }
+
+                }
                 
-                MonthlyEmpWages = empHour * Wage_Per_Hour ;
-          }
-                Console.WriteLine("Monthly employee wage is :- " + MonthlyEmpWages);
-          
-        }
+                TotalHrs = TotalHrs + empHour;
+           }
+            Console.WriteLine("Total working hours in month are :- " + TotalHrs);
+            MonthlyEmpWages = TotalHrs * Wage_Per_Hour;
+            Console.WriteLine("Monthly employee wage is :- " + MonthlyEmpWages)          
         }
     }
 }
